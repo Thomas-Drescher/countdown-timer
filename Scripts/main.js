0121da908e68849e3d71eb0b351c27c322f4b5c0
Tom Drescher <tmdrescher@yahoo.com>
2017-10-08 17:43:27 -0400

    enable custom form

--- Scripts/main.js
+++ Scripts/main.js
@@ -4,6 +4,8 @@ const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
   const now = Date.now();
   const then = now + seconds * 1000;
   // initial display
@@ -44,3 +46,9 @@ function startTimer() {
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  timer(mins * 60);
+  this.reset();
+});
