71b5386c0bb9b37d0ca62abb85152ca0b8608ba6
Tom Drescher <tmdrescher@yahoo.com>
2017-10-08 15:47:39 -0400

    connect html display

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,6 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+
 
 function timer(seconds) {
   const now = Date.now();
@@ -21,5 +23,6 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  console.log(minutes, remainderSeconds);
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  timerDisplay.textContent = display;
 }
